a07acc604b7425d4c2452ddad912d8f653c12d8e
krishna thakur <115836360+krsnaimp@users.noreply.github.com>
2024-01-24 22:31:04 +0530

    why there is need of multithreading ?

--- app/src/main/java/com/example/coroutinestest/MainActivity.kt
+++ app/src/main/java/com/example/coroutinestest/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.coroutinestest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var showMessage : Button
+    private lateinit var startTask : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        showMessage = findViewById(R.id.showMessage)
+        startTask = findViewById(R.id.StartTask)
+
+        showMessage.setOnClickListener(){
+
+            Toast.makeText(this, "Button is Clicked", Toast.LENGTH_SHORT).show()
+        }
+
+        startTask.setOnClickListener(){
+            //starts show long running task which potentially blocks the ui
+            for (i in 0..10000000){
+                Log.i("okay",i.toString())
+            }
+        }
     }
 }
