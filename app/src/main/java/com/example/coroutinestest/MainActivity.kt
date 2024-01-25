4daa7df996bc966498beecfb574290930559dc66
krishna thakur <115836360+krsnaimp@users.noreply.github.com>
2024-01-25 12:19:15 +0530

    switching the context from background thread to main because ui i.e textview can not be in background thread

--- app/src/main/java/com/example/coroutinestest/MainActivity.kt
+++ app/src/main/java/com/example/coroutinestest/MainActivity.kt
@@ -5,15 +5,18 @@ import android.os.Bundle
 import android.provider.Settings.Global
 import android.util.Log
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var showMessage : Button
     private lateinit var startTask : Button
+    private lateinit var taskCounter : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +24,7 @@ class MainActivity : AppCompatActivity() {
 
         showMessage = findViewById(R.id.showMessage)
         startTask = findViewById(R.id.StartTask)
+        taskCounter = findViewById(R.id.TaskCounter)
 
         showMessage.setOnClickListener(){
 
@@ -31,7 +35,9 @@ class MainActivity : AppCompatActivity() {
             //starts show long running task which potentially blocks the ui
             GlobalScope.launch(Dispatchers.IO) {
                 for (i in 0..10000000){
-                    Log.i("okay",i.toString())
+                withContext(Dispatchers.Main){
+                    taskCounter.text = i.toString()
+                }
                 } // using coroutines to solve the problem
             }
 
